dd6696f4aad8015cbac44e1ba60ca6a32272d31e
Hien Phan The <hien.phanthe@pycogroup.com>
2017-08-11 13:00:14 +0700

    add checking balance

--- src/service/JeniusService.js
+++ src/service/JeniusService.js
@@ -1,18 +1,32 @@
-const { apiToJson } = require('./api');
+import api from './apiClient';
+
 const Pattern = require('../model/pattern');
 
 class JeniusService {
 
-  totalNumberOfTeams() {
+  _totalNumberOfTeams() {
     return Promise.resolve(12);
+  }
+
+  _getCurrentBalance() {
+    return api.getCurrentBalance('90010000526').then(data => data.balance);
   }
 
   getPatternMatches() {
     const patterns = [];
-    patterns.push(new Pattern([/^teams$/],
-      () => this.totalNumberOfTeams()
+
+    const team = new Pattern([/^teams$/],
+      () => this._totalNumberOfTeams()
         .then((number) => `Number of TEAMS = ${number}`)
-      ));
+    );
+    patterns.push(team);
+
+    const currentBalance = new Pattern([/balance/],
+      () => this._getCurrentBalance()
+        .then(balance => `Your current balance is ${balance}`)
+    );
+
+    patterns.push(currentBalance);
 
     return patterns;
   }
